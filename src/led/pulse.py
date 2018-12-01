b272dddefa61e5b30a4a4ce9684d68c07507dec6
Richard Birkby <rbirkby@gmail.com>
2018-12-01 17:49:10 +0000

    Add a background thread

--- src/led/pulse.py
+++ src/led/pulse.py
@@ -4,6 +4,7 @@ import colorsys
 import time
 from flask import Flask
 from sys import exit
+import threading
 
 try:
     import numpy as np
@@ -13,7 +14,8 @@ except ImportError:
 import ledshim
 
 ledshim.set_clear_on_exit()
-
+lock = threading.Lock()
+hue = 0.5
 app = Flask(__name__)
 
 @app.route("/health")
@@ -23,37 +25,42 @@ def health():
 
 @app.route("/display/a", methods=['POST'])
 def a():
+    global hue
     print "Received a update a"
-    ledshim.set_all(128, 0, 0)
-    ledshim.show()
+    with lock:
+        hue = 0
     return "OK"
 
 @app.route("/display/b", methods=['POST'])
 def b():
+    global hue
     print "Received a update b"
-    ledshim.set_all(0, 128, 0)
-    ledshim.show()
+    with lock:
+        hue = 0.2
     return "OK"
 
 @app.route("/display/c", methods=['POST'])
 def c():
+    global hue
     print "Received a update c"
-    ledshim.set_all(0, 0, 128)
-    ledshim.show()
+    with lock:
+        hue = 0.4
     return "OK"
 
 @app.route("/display/d", methods=['POST'])
 def d():
+    global hue
     print "Received a update d"
-    ledshim.set_all(128, 128, 0)
-    ledshim.show()
+    with lock:
+        hue = 0.6
     return "OK"
 
 @app.route("/display/e", methods=['POST'])
 def e():
-    print "Received a update 3"
-    ledshim.set_all(0, 128, 128)
-    ledshim.show()
+    global hue
+    print "Received a update e"
+    with lock:
+        hue = 0.8
     return "OK"
 
 def make_gaussian(fwhm):
@@ -64,25 +71,28 @@ def make_gaussian(fwhm):
     gauss = np.exp(-4 * np.log(2) * ((x - x0) ** 2 + (y - y0) ** 2) / fwhm ** 2)
     return gauss
 
+def pulse():
+    while True:
+        for z in list(range(1, 10)[::-1]) + list(range(1, 10)):
+            fwhm = 15.0 / z
+            gauss = make_gaussian(fwhm)
+            start = time.time()
+            y = 4
+            with lock:
+                for x in range(ledshim.NUM_PIXELS):
+                    h = hue
+                    s = 1.0
+                    v = gauss[x, y]
+                    rgb = colorsys.hsv_to_rgb(h, s, v)
+                    r, g, b = [int(255.0 * i) for i in rgb]
+                    ledshim.set_pixel(x, r, g, b)
+                ledshim.show()
+            end = time.time()
+            t = end - start
+            if t < 0.04:
+                time.sleep(0.04 - t)
 
-# while True:
-#     for z in list(range(1, 10)[::-1]) + list(range(1, 10)):
-#         fwhm = 15.0 / z
-#         gauss = make_gaussian(fwhm)
-#         start = time.time()
-#         y = 4
-#         for x in range(ledshim.NUM_PIXELS):
-#             h = 0.5
-#             s = 1.0
-#             v = gauss[x, y]
-#             rgb = colorsys.hsv_to_rgb(h, s, v)
-#             r, g, b = [int(255.0 * i) for i in rgb]
-#             ledshim.set_pixel(x, r, g, b)
-#         ledshim.show()
-#         end = time.time()
-#         t = end - start
-#         if t < 0.04:
-#             time.sleep(0.04 - t)
+pulseThread = threading.Thread(name='pulseThread', target=pulse)
+pulseThread.start()
 
-if __name__ == '__main__':
-    app.run()
+app.run(host="0.0.0.0")
